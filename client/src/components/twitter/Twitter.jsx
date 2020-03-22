824eb08ad24bf332dbe912ba768d871022c5e1f6
villatrue <scarter0313@gmail.com>
2020-03-21 23:58:23 -0400

    twitter

---
+++ client/src/components/twitter/Twitter.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Twitter = () => {
+    return (
+        <div className={'tile-container'}>            
+            <p>Twitter</p>
+        </div>
+    )
+};
+
+export default Twitter;
